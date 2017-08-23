8ec986c00683eda4513d502db8f1babe210c4efc
prach-imad <31194614+prach-imad@users.noreply.github.com>
2017-08-23 18:59:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/article-one', function (req, res)
-{res.send("Article one requested and will be served here");
+app.get('/article-one', function (req, res){
+  res.sendFile(path.join(__dirname, 'ui', 'article 1.html'));
 });
 app.get('/article-two', function (req, res)
 {res.send("Article two requested and will be served here");
