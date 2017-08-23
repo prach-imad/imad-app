7290d5ddc92b5bd7d755ceab44eab058f34abefd
prach-imad <31194614+prach-imad@users.noreply.github.com>
2017-08-23 20:16:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,8 @@
 console.log('Loaded!');
 var element = document.getElementById("main text");
 element.innerHTML = "New Value";
-var image=document.getElementById("madi");
-img.onclick = function()
-{
-    img.style.marginLeft="100px";
-};
+//var image=document.getElementById("madi");
+//img.onclick = function()
+//{
+  //  img.style.marginLeft="100px";
+//};
